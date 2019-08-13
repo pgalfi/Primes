1ab2aa1773369262e030fe28c9a366ae3e779c56
Peter Galfi <pgalfi@gmail.com>
2019-08-14 00:48:58 +0200

    adjustments to prime generator service signature and tests

--- Primes/settings.py
+++ Primes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bx9-+40fw(fn-#3ttbe(28g^te4p^c01s)=l)hydj75l*p+r$v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
