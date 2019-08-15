420334c43c2bbe77248e9b8cae72e1ae0d9507a2
Peter Galfi <pgalfi@gmail.com>
2019-08-15 21:13:14 +0200

    implemented improved concurrency handling

--- primenumbers/services/primes.py
+++ primenumbers/services/primes.py
@@ -1,35 +1,71 @@
+import time
 from math import sqrt, trunc
 from threading import Lock
 
 from bitarray import bitarray
 
-primes = bitarray([False, False])  # numbers 0 and 1 are not prime
-bitarray_access = Lock()
+primes_sieve = {
+    "array": bitarray([False, False]),  # numbers 0 and 1 are not prime
+    "prev_n": 1,
+    "access": Lock(),
+}
 
 
-def _primes_list(n, start=1):
-    if n < 2:
-        return []
-    prev_n = primes.length() - 1
-    bitarray_access.acquire()
-    if n > prev_n:
-        part = bitarray(n - prev_n)
-        part.setall(True)
-        primes.extend(part)
+def build_sieve(n):
+    primes = primes_sieve["array"]
+    print("allocating: ", primes_sieve["prev_n"], "-", n)
+    part = bitarray(n - primes_sieve["prev_n"])
+    part.setall(True)
+    primes.extend(part)
+    prev_n = primes_sieve["prev_n"]
     for i in range(2, int(sqrt(n)) + 1):
         if primes[i] is True:
             for j in range(max(i * i, i * (trunc(prev_n / i) + 1)), n + 1, i):
                 primes[j] = False
-    bitarray_access.release()
+    primes_sieve["prev_n"] = n
+
+
+def apply_sieve(n, start):
+    if n < 2:
+        return []
+    primes = primes_sieve["array"]
     return [i for i in range(start, n) if primes[i]]
 
 
-def get_primes(n, start=1, page=1, page_size=None):
-    primes_list = _primes_list(n, start)
+def _primes_list_optional_lock(n, start=1):
+    while n > primes_sieve["prev_n"]:
+        if primes_sieve["access"].locked():
+            time.sleep(0.1)
+        else:
+            primes_sieve["access"].acquire()
+            build_sieve(n)
+            primes_sieve["access"].release()
+
+    return apply_sieve(n, start)
+
+
+def _primes_list_always_lock(n, start=1):
+    primes_sieve["access"].acquire()
+    if n > primes_sieve["prev_n"]:
+        build_sieve(n)
+    primes_sieve["access"].release()
+    return apply_sieve(n, start)
+
+
+def paginate(page, page_size, primes_list, start):
     if page_size is None:
         return {"numbers": primes_list, "count": len(primes_list), "start": start, "page": page, "page_size": page_size}
     return {"numbers": primes_list[(page - 1) * page_size: page * page_size], "count": len(primes_list),
             "start": start, "page": page, "page_size": page_size}
+
+
+def get_primes(n, start=1, page=1, page_size=None, locking="always"):
+    if locking == "optional":
+        primes_list = _primes_list_optional_lock(n, start)
+    else:
+        primes_list = _primes_list_always_lock(n, start)
+
+    return paginate(page, page_size, primes_list, start)
 
 
 # Below versions are obsolete, were kept only for testing and performance comparisons
--- primenumbers/tests/test_get_primes_concurrent.py
+++ primenumbers/tests/test_get_primes_concurrent.py
@@ -1,4 +1,5 @@
 import threading
+import time
 from unittest import TestCase
 
 from primenumbers.services.primes import get_primes
@@ -31,14 +32,85 @@ def test_concurrently(times):
     return test_concurrently_decorator
 
 
+def concurrent_with_params(*params):
+    start = time.time()
+    def wrapped_with_arguments(test_method):
+
+        def wrapper(*args, **kwargs):
+            exceptions = []
+            threads = []
+
+            def call_with_thread(*arguments):
+                print("called service with", arguments[1:])
+                try:
+                    test_method(*arguments)
+                except Exception as e:
+                    exceptions.append(e)
+                    raise
+                end = time.time()
+                print(arguments[1:], ">>>", end-start)
+            if params is None:
+                threads.append(threading.Thread(target=call_with_thread))
+            else:
+                for parameter in params:
+                    threads.append(threading.Thread(target=call_with_thread, args=(args[0], parameter)))
+
+            for t in threads: t.start()
+            for t in threads: t.join()
+            if exceptions:
+                raise Exception('test_concurrently intercepted %s exceptions: %s' % (len(exceptions), exceptions))
+
+        return wrapper
+
+    return wrapped_with_arguments
+
+
 class TestGetPrimesConcurrent(TestCase):
 
     @test_concurrently(5)
     def test_concurrent_01(self):
-        self.assertEqual(78498, get_primes(1000000)["count"])  # 1s 202ms -- one time execution time is 600ms
+        self.assertEqual(78498, get_primes(1000000, locking="always")["count"])
+        # 1s 200ms -- one time execution time is 600ms
 
     @test_concurrently(10)
     def test_concurrent_02(self):
-        self.assertEqual(78498, get_primes(1000000)["count"])  # 1s 475ms -- one time execution is 600ms
+        self.assertEqual(78498, get_primes(1000000, locking="always")["count"])
+        # 1s 800ms -- one time execution is 600ms
 
+    def test_sequential(self):
+        self.assertGreater(get_primes(500000)["count"], 0)
+        self.assertGreater(get_primes(10000000)["count"], 0)
+        self.assertGreater(get_primes(400000)["count"], 0)
+        self.assertGreater(get_primes(300000)["count"], 0)
+        # 6.45s -- sequential, no concurrency
+
+    @concurrent_with_params(500000, 10000000, 400000, 300000)
+    def test_concurrent_always(self, n):
+        self.assertGreater(get_primes(n, locking="always")["count"], 0)
+        # Output:
+        # called service with (500000,)
+        # allocating:  1 - 500000
+        # called service with (10000000,)
+        # called service with (400000,)
+        # called service with (300000,)
+        # allocating:  500000 - 10000000
+        # (500000,) >>> 0.6092565059661865
+        # (300000,) >>> 5.083366394042969
+        # (400000,) >>> 5.098953008651733
+        # (10000000,) >>> 6.410214185714722
+
+    @concurrent_with_params(500000, 10000000, 400000, 300000)
+    def test_concurrent_optional(self, n):
+        self.assertGreater(get_primes(n, locking="optional")["count"], 0)
+        # Output:
+        # called service with (500000,)
+        # allocating:  1 - 500000
+        # called service with (10000000,)
+        # called service with (400000,)
+        # called service with (300000,)
+        # allocating:  500000 - 10000000
+        # (500000,) >>> 0.6332592964172363
+        # (300000,) >>> 0.666248083114624
+        # (400000,) >>> 0.7211008071899414
+        # (10000000,) >>> 6.694701194763184
 
